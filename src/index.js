9aed57989cc594b2c00c7b2e4503d33241e7d83e
jflavadre <johnfernando10@hotmail.com>
2024-06-02 21:42:18 +0900

    add process env PORT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import app from "./server/app.js";
 import { connectDB } from "./database/database.js";
+//---- Port Binding
+const port = process.env.PORT || 4000;
 
 //MongoDB Atlasに接続
 connectDB();
 
-app.listen(3000);
-console.log("サーバを起動しました: ", 3000);
+app.listen(port, () => {
+	console.log("サーバを起動しました: ", port);
+});
